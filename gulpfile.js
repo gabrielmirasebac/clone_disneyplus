80d07e8da22dcb9bf560b3d8579d79f046e12ed3
Gabriel Miras Prevideli <gabriel.prevideli@gmail.com>
2024-05-12 14:31:03 -0300

    atualizacao arquivos imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles(){
     return gulp.src('./src/styles/*.scss') //recupera os arquivos desejados para compilacao
@@ -7,7 +8,14 @@ function styles(){
     .pipe(gulp.dest('./dist/css')) //destino dos arquivos comprimidos
 }
 
-exports.default = styles;
+function images(){
+    return gulp.src('./src/images/**/*') //recupera os arquivos desejados para compilacao
+    .pipe(imagemin()) //estilo de saido do sass vai ser comprimido
+    .pipe(gulp.dest('./dist/images')) //destino dos arquivos comprimidos
+}
+
+exports.default = gulp.parallel(styles,images);
+
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel([styles]))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
