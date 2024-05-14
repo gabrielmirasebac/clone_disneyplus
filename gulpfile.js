47881c5daaaee26f730a6522e22927b2bd5ee089
Gabriel Miras Prevideli <gabriel.prevideli@gmail.com>
2024-05-14 00:44:02 -0300

    criando o hero e importando as fontes Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss') //recupera os arquivos desejados para compilacao
-    .pipe(sass({ outputStyle:'compressed'})) //estilo de saido do sass vai ser comprimido
-    .pipe(gulp.dest('./dist/css')) //destino dos arquivos comprimidos
+        .pipe(sass({ outputStyle:'compressed'})) //estilo de saido do sass vai ser comprimido
+        .pipe(gulp.dest('./dist/css')); //destino dos arquivos comprimidos
 }
 
 function images(){
     return gulp.src('./src/images/**/*') //recupera os arquivos desejados para compilacao
-    .pipe(imagemin()) //estilo de saido do sass vai ser comprimido
-    .pipe(gulp.dest('./dist/images')) //destino dos arquivos comprimidos
+        .pipe(imagemin()) //estilo de saido do sass vai ser comprimido
+        .pipe(gulp.dest('./dist/images')); //destino dos arquivos comprimidos
 }
 
 exports.default = gulp.parallel(styles,images);
